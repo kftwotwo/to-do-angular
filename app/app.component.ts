2255719dc3e5664cfe72d11dd54b1d38f921ba61
Kevin Finley <kevin@>
2017-01-17 10:03:40 -0800

    done with the 'to do list'

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,13 +5,27 @@ import { Component } from '@angular/core';
   template: `
     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
     <h3>{{list}}</h3>
+    <ul>
+       <li *ngFor="let currentTask of tasks">{{currentTask.description}}</li>
+    </ul>
   `
 })
 
 export class AppComponent {
-  list: string = 'list will be here soon';
+  list: string = 'list ';
   currentTime = new Date();
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
+  tasks: Task[] = [
+    new Task('add readme content'),
+    new Task('wash the car'),
+    new Task('mow the lawn'),
+    new Task('clean house')
+  ];
+}
+
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string){}
 }
