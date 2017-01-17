b2e6f0aa59d342a2aa49102a5049ec034f3dd5bf
Kevin Finley <kevin@>
2017-01-17 09:42:02 -0800

    add date for to do list

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>This is a import statement </h1>
+    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+    <h3>{{list}}</h3>
   `
 })
 
 export class AppComponent {
-
+  list: string = 'list will be here soon';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
 }
--- app/to-do.ts
+++ app/to-do.ts
@@ -2,6 +2,10 @@ class Task {
   done: boolean = false;
 
   constructor(public description: string, public priority: string) {}
+
+  markDone(){
+    this.done = true;
+  }
 }
 
 var tasks: Task[] = [];
@@ -9,6 +13,8 @@ tasks.push(new Task('Do the dishes Kevin', 'High'));
 tasks.push(new Task('Pick up dog poop', 'Low'));
 tasks.push(new Task('Take trash out', 'High'));
 tasks.push(new Task('Game on h1z1', 'Highest'));
+
+tasks[1].markDone();
 
 for(var task of tasks){
   console.log(task)
